311b17a2a3bfa1db0143c5e031db83fc4c1edbae
Menna Alaa <menaalaa460@gmail.com>
2026-09-13 13:01:23 +0300

    Q2 P

--- Program.cs
+++ Program.cs
@@ -36,40 +36,157 @@
 //User Story: You are building a simple Smart Delivery Management System for a delivery company. The system man-ages shipment data,
 //delivery addresses, price and shipment searching. Build it as a Console Application that reads data from the user and prints shipment information.
 
-//struct DeliveryAddress
+struct DeliveryAddress
+{
+    public string City;
+    public string Street;
+    public int BuildingNumber;
+
+    public DeliveryAddress(string city, string street, int buildingNumber)
+    {
+        City = city;
+        Street = street;
+        BuildingNumber = buildingNumber;
+    }
+    public string GetFullAddress()
+    {
+        return $"{BuildingNumber} {Street}, {City}";
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        DeliveryAddress address1 =
+            new DeliveryAddress("Cairo", "Maadi Street", 233);
+        DeliveryAddress address2 = address1;
+        address2.City = "Cairo";
+        address2.Street = " october ";
+        address2.BuildingNumber = 20;
+
+        Console.WriteLine("Original Address: " + address1.GetFullAddress());
+
+        Console.WriteLine("Copied Address: " + address2.GetFullAddress());
+    }
+}
+#endregion
+
+#region 2.Ceate a Shipment struct
+
+//struct Shipment
 //{
-//    public string City;
-//    public string Street;
-//    public int BuildingNumber;
-
-//    public DeliveryAddress(string city, string street, int buildingNumber)
+//    private string trackingCode;
+//    private string description;
+//    private double weight;
+//    private decimal deliveryFee;
+//    public string TrackingCode
 //    {
-//        City = city;
-//        Street = street;
-//        BuildingNumber = buildingNumber;
+//        get
+//        {
+//            return trackingCode;
+//        }
+//        private set
+//        {
+//            if (!string.IsNullOrWhiteSpace(value))
+//                trackingCode = value;
+//        }
 //    }
-//    public string GetFullAddress()
+//    public string Description
 //    {
-//        return $"{BuildingNumber} {Street}, {City}";
+//        get
+//        {
+//            return description;
+//        }
+//        set
+//        {
+//            if (!string.IsNullOrWhiteSpace(value))
+//                description = value;
+//        }
 //    }
-//}
-//class Program
-//{
-//    static void Main(string[] args)
+//    public double Weight
 //    {
-//        DeliveryAddress address1 =
-//            new DeliveryAddress("Cairo", "Maadi Street", 233);
-//        DeliveryAddress address2 = address1;
-//        address2.City = "Cairo";
-//        address2.Street = " october ";
-//        address2.BuildingNumber = 20;
+//        get
+//        {
+//            return weight;
+//        }
+//        set
+//        {
+//            if (value > 0)
+//                weight = value;
+//        }
+//    }
 
-//        Console.WriteLine("Original Address: " + address1.GetFullAddress());
+//    public decimal DeliveryFee
+//    {
+//        get
+//        {
+//            return deliveryFee;
+//        }
+//        private set
+//        {
+//            if (value > 0)
+//                deliveryFee = value;
+//        }
+//    }
 
-//        Console.WriteLine("Copied Address: " + address2.GetFullAddress());
+//    public DeliveryAddress Destination { get; set; }
+
+//    public decimal EstimatedCost
+//    {
+//        get
+//        {
+//            return DeliveryFee + ((decimal)Weight * 5);
+//        }
+//    }
+
+   
+//    public Shipment(string trackingCode)
+//    {
+//        this.trackingCode = "";
+//        description = "Unknown";
+//        weight = 1;
+//        deliveryFee = 50;
+//        Destination = new DeliveryAddress("Unknown", "Unknown", 0);
+
+//        TrackingCode = trackingCode;
+//    }
+
+//    public Shipment(
+//        string trackingCode,
+//        string description,
+//        double weight,
+//        decimal deliveryFee,
+//        DeliveryAddress destination)
+//    {
+//        this.trackingCode = "";
+//        this.description = "Unknown";
+//        this.weight = 1;
+//        this.deliveryFee = 50;
+//        Destination = destination;
+
+//        TrackingCode = trackingCode;
+//        Description = description;
+//        Weight = weight;
+//        DeliveryFee = deliveryFee;
+//    }
+
+//    public void UpdateDeliveryFee(decimal newFee)
+//    {
+//        if (newFee > 0)
+//            DeliveryFee = newFee;
+//    }
+//    public void PrintShipment()
+//    {
+//        Console.WriteLine("Tracking Code: " + TrackingCode);
+//        Console.WriteLine("Description: " + Description);
+//        Console.WriteLine("Weight: " + Weight + " KG");
+//        Console.WriteLine("Delivery Fee: " + DeliveryFee + " EGP");
+//        Console.WriteLine("Destination: " + Destination.GetFullAddress());
+//        Console.WriteLine("Estimated Cost: " + EstimatedCost + " EGP");
 //    }
 //}
 #endregion
+
 
 
 
