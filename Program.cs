a820d88ebfbecb7a39fd7f6419a2f11ac4157a75
Menna Alaa <menaalaa460@gmail.com>
2026-09-13 11:02:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿#region
+public struct DeliveryAddress
+{
+    public string City; public string Street;
+}
+public class Customer
+{
+    public string Name;
+}
+a)	 What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
+    ..Because DeliveryAddress is a struct (value type), copying it creates an independent copy. 
+    Modifying the copied variable does not affect the original variable.
