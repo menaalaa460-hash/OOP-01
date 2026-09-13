1a7d62cece4a704d326a9f0c293047730db3bf75
Menna Alaa <menaalaa460@gmail.com>
2026-09-13 11:14:53 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -10,4 +10,11 @@
 //a)	 What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
 //    ..Because DeliveryAddress is a struct (value type), copying it creates an independent copy. 
 //    Modifying the copied variable does not affect the original variable.
-   #endregion
+
+//B)b) What happens when a Customer variable is copied into another variable and one variable modifies the objec
+//Because Customer is a class (reference type), copying the variable copies the reference to the same
+#endregion
+
+
+
+
