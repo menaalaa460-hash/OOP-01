b14cd3201eb13a762270f48bb12cedab0206dbae
Menna Alaa <menaalaa460@gmail.com>
2026-09-13 12:22:55 +0300

    Q2

--- Program.cs
+++ Program.cs
@@ -28,12 +28,48 @@
 //   3-The internal data is not protected
 
 //b)	b) How can private fields and public properties improve this design ?
-    // Private fields protect the data.
-    // Public properties provide controlled access and validation.
+// Private fields protect the data
+// Public properties provide controlled access and validation
 #endregion
 
+#region Practical
+//User Story: You are building a simple Smart Delivery Management System for a delivery company. The system man-ages shipment data,
+//delivery addresses, price and shipment searching. Build it as a Console Application that reads data from the user and prints shipment information.
 
+//struct DeliveryAddress
+//{
+//    public string City;
+//    public string Street;
+//    public int BuildingNumber;
 
+//    public DeliveryAddress(string city, string street, int buildingNumber)
+//    {
+//        City = city;
+//        Street = street;
+//        BuildingNumber = buildingNumber;
+//    }
+//    public string GetFullAddress()
+//    {
+//        return $"{BuildingNumber} {Street}, {City}";
+//    }
+//}
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        DeliveryAddress address1 =
+//            new DeliveryAddress("Cairo", "Maadi Street", 233);
+//        DeliveryAddress address2 = address1;
+//        address2.City = "Cairo";
+//        address2.Street = " october ";
+//        address2.BuildingNumber = 20;
+
+//        Console.WriteLine("Original Address: " + address1.GetFullAddress());
+
+//        Console.WriteLine("Copied Address: " + address2.GetFullAddress());
+//    }
+//}
+#endregion
 
 
 
